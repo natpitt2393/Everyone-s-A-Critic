b6c7aa520899324fb005e7308b32b55bb836dbc0
namagaii <rnoel2125@gmail.com>
2021-11-19 02:35:57 -0500

    Page now generates result cards and can flip between pages of them.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,14 @@
+const MAXCARDSPERPAGE = 3;
+const PLACEHOLDERIMG = "https://via.placeholder.com/150"
 // Capture search info
 let resultPageURl = "./results.html";
 let searchData;
 let currentPage;
 let numOfPages;
-const MAXCARDSPERPAGE = 3;
+// Effective Enum with two values: Search and Results
+let currentPageLayout = "Search"; //Default value is search
+SwitchPageLayout();
+
 $("#SearchForm").submit(OnSubmit)
 function OnSubmit(event){
     event.preventDefault();
@@ -13,8 +18,37 @@ function OnSubmit(event){
     FetchData(movieName, year);
 }
 
+$("#backbtn").on("click", DisplayPreviousPage);
+
+$("#nextbtn").on("click", DisplayNextPage);
+
+$("pageNumContainer").on("click", function(event){
+    event.preventDefault();
+    if (event.target.tagName === "A"){
+        MoveToPage(event.target.id);
+    }
+    else{
+        console.log("Idk events be complex")
+    }
+});
+
+function SwitchPageLayout(){
+    if (currentPageLayout === "Search"){
+        $("#searchSection").attr("class", "searchbox");
+        $("#navBtns").attr("class", "hide");
+        $("#movieSection").attr("class", "hide");
+    } else if (currentPageLayout === "Results"){
+        $("#movieSection").attr("class", "movie-section");
+        $("#navBtns").attr("class", "navbtns");
+        $("#searchSection").attr("class", "hide");
+    } else {
+        console.error("Invalid Page Layout Value");
+        //TODO: Deal with error
+    }
+}
+
 function FetchData(p_name, p_year){
-    let requestURL = "https://api.themoviedb.org/3/search/movie?api_key=cba89e07597df25af6057ff006d6ebc5&query=Saw";
+    let requestURL = "https://api.themoviedb.org/3/search/movie?api_key=cba89e07597df25af6057ff006d6ebc5&";
     requestURL += "query="+p_name;
    
     fetch(requestURL)
@@ -22,10 +56,14 @@ function FetchData(p_name, p_year){
             return response.json();
         })
         .then(function(data){
+            console.log(data)
+            currentPage = 1;
+            currentPageLayout = "Results";
             searchData = data;
             numOfPages = Math.ceil(data.results.length / MAXCARDSPERPAGE);
             displaySearchData(0);
-            currentPage = 1;
+            DisplayPageNumbers();
+            SwitchPageLayout();
         })
             
            
@@ -36,25 +74,52 @@ function FetchData(p_name, p_year){
 }
 
 function displaySearchData(p_startIndex) {
+    $("#movieSection").children().remove();
     //for every result first create a new card for it. 
     // For every new card display all pertinent info about movie.
     // overview (plot), release_date, title, and poster
-    for (let i = p_startIndex; i < MAXCARDSPERPAGE; i++) {
-       if (!searchData) {console.warn("Search data is null"); return;}
-        if (!searchData.results[i]) { return;}
+    for (let i = p_startIndex; i < MAXCARDSPERPAGE+p_startIndex; i++) {
+       if (!searchData) { console.warn("Search data is null"); return; } 
+        if (!searchData.results[i]) { return; }
+        console.log(searchData.results[i]);
         let _card = generateCard();
-       _card.posterImageEl.attr("src", "https://themoviedb.org/t/p/w1280/" + searchData.results[i].poster_path);
-       _card.posterImageEl.attr("alt", "The poster for the movie, " + searchData.results[i].title + ".");
+        if (searchData.results[i].poster_path != null){
+            _card.posterImageEl.attr("src", "https://themoviedb.org/t/p/w1280/" + searchData.results[i].poster_path);
+            _card.posterImageEl.attr("alt", "The poster for the movie, " + searchData.results[i].title + ".");
+        }else{
+            _card.posterImageEl.attr("src", PLACEHOLDERIMG);
+            _card.posterImageEl.attr("alt", "Was supposed to be a poster for the movie, " + searchData.results[i].title + " but it an image was not found.");
+        }
        _card.movieNameEl.text(searchData.results[i].title);
        _card.movieDateEl.text(searchData.results[i].release_date);
        _card.movieOverviewEl.text(searchData.results[i].overview);
-       $("card-section").append(_card.cardEl);
+       _card.wrapperAnchor.attr("id", ""+i);
+       AddCardClickEvent(_card.wrapperAnchor);
+       $("#movieSection").append(_card.cardEl);
     }
 }
 
-//returns object storing elements: cardEl, posterContainerEl, figureEl,
+
+// TODO:  Restrict max page numbers at once and "scroll" the numbers depending on position
+function DisplayPageNumbers(){
+    for (let i = 1; i <= numOfPages; i++){
+        //Create number element
+        let numberEl = $("<a>");
+        if(i === currentPage){
+            numberEl.attr("id", "currentnum");
+        } else {
+            numberEl.attr("id", i+"");
+            numberEl.attr("class", "pagenum");
+        }
+        numberEl.text(""+i);
+        $("#pageNumContainer").append(numberEl);
+    }
+}
+
+//returns object storing elements: wrapperAnchor, cardEl, posterContainerEl, figureEl,
 //posterImageEl, movieNameEl, movieDateEl, movieOverviewEl
 function generateCard() {
+    let _wrapperAnchor = $("<a>");
     let _card = $("<div>");
     _card.attr("class", "movie-card");
     let _posterContainer = $("<div>");
@@ -73,7 +138,9 @@ function generateCard() {
     _movieOverview.attr("class", "movie-overview");
     _cardContentContainer.append([_movieName, _movieDate, _movieOverview]);
     _card.append([_posterContainer, _cardContentContainer]);
+    _wrapperAnchor.append(_card);
     let _cardObject = {
+        wrapperAnchor: _wrapperAnchor,
         cardEl: _card,
         posterContainerEl: _posterContainer,
         figureEl: _figure,
@@ -87,17 +154,21 @@ function generateCard() {
 
 //For on next button clicked
 function DisplayNextPage(){
+    //Should call unless its displayed
+    if(currentPageLayout !== "Results"){return;}
     //Make sure we don't leave range
     if(currentPage + 1 > numOfPages){
         return;
     }
-    displaySearchData((currentPage*MAXCARDSPERPAGE)-MAXCARDSPERPAGE);
     currentPage++;
+    displaySearchData((currentPage*MAXCARDSPERPAGE)-MAXCARDSPERPAGE);
     CheckPagesLeft();
 }
 
 //For on back button clicked
 function DisplayPreviousPage(){
+    //Should call unless its displayed
+    if(currentPageLayout !== "Results"){return;}
     //Make sure we don't leave range
     if (currentPage - 1 < 1){
         return;
@@ -107,15 +178,45 @@ function DisplayPreviousPage(){
     CheckPagesLeft();
 }
 
+//Fix enable code and fix backBtn disable condition
 function CheckPagesLeft(){
+    let _nextBtn = $("#nextbtn");
+    let _backBtn = $("#backbtn");
     if (currentPage + 1 > numOfPages){
         //Disable Next Button
+        _nextBtn.prop("disabled", true);
+        console.log("ButtonDisabled")
     } else if (currentPage - 1 < 1) {
         //Disable Back Button
+        _backBtn.prop("disabled", true);
+        console.log("ButtonDisabled")
+    }
+    if (_nextBtn.prop("disabled") && !currentPage + 1 > numOfPages){
+        //Enable Next Button if disabled
+        _nextBtn.prop("disabled", false);
+    }
+    if (_backBtn.prop("disabled") && !currentPage - 1 < 1){
+        //Enable Back Button if disabled
+        _backBtn.prop("disabled", false);
     }
 }
 
 //For on click of the page numbers
 function MoveToPage(p_pageNum){
+    if (p_pageNum <= 0 || p_pageNum > numOfPages){
+        console.warn("Attempted to move to page out of range.")
+        return;
+    }
+    console.log("MoveToPage")
     displaySearchData((p_pageNum*MAXCARDSPERPAGE)-MAXCARDSPERPAGE);
+    currentPage = p_pageNum;
+}
+
+function AddCardClickEvent(p_wrapperAnchor){
+    p_wrapperAnchor.addEventListener("click", OnCardClicked);
+}
+
+function OnCardClicked(event){
+    event.preventDefault();
+    localStorage.setItem("TMDBSearchData", JSON.stringify(searchData.results[event.target.id]));
 }
